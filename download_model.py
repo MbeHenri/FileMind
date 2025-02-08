dc9d1debbef2992bd80c06bb5f25d4cff697ad8f
MbeHenri <henri.mbe@facsciences-uy1.cm>
2025-02-08 15:07:46 +0100

    add testing of deepseek

---
+++ download_model.py
@@ -0,0 +1,15 @@
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+# Load the model and tokenizer from Hugging Face
+model_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
+dir_name = "./models/deepseek"
+
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+model.save_pretrained(dir_name)
+tokenizer.save_pretrained(dir_name)
+
+# Load the model and tokenizer from the local directory
+# local_model = AutoModelForSequenceClassification.from_pretrained(dir_name)
+# local_tokenizer = AutoTokenizer.from_pretrained(dir_name)
